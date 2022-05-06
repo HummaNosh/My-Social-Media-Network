9ca3aa0f6da6eff2ec3f1c0505eff6da269d58f9
Humma Noshine <Hummanoshine@gmail.com>
2022-05-06 13:27:55 +0100

    trying to control..

---
+++ controllers/userController.js
@@ -0,0 +1,114 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thoughts } = require('../models');
+
+// Aggregate function to get the number of students overall
+// const headCount = async () =>
+//   Student.aggregate()
+//     .count('studentCount')
+//     .then((numberOfStudents) => numberOfStudents);
+
+// // Aggregate function for getting the overall grade using $avg
+// const grade = async (studentId) =>
+//   Student.aggregate([
+//     // only include the given student by using $match
+//     { $match: { _id: ObjectId(studentId) } },
+//     {
+//       $unwind: '$assignments',
+//     },
+//     {
+//       $group: {
+//         _id: ObjectId(studentId),
+//         overallGrade: { $avg: '$assignments.score' },
+//       },
+//     },
+//   ]);
+
+module.exports = {
+
+  getallUsers(req, res) {
+    User.find()
+      .then(async (User) => {
+        const UserOB = {
+          User,
+        //   headCount: await headCount(),
+        };
+        return res.json(UserOB);
+      })
+      .catch((err) => {
+        console.log("borked", err);
+        return res.status(500).json(err);
+      });
+  },
+
+  getallUsers(req, res) {
+    User.findOne({ _id: req.params.userID })
+      .select('-__v')
+      .then(async (User) =>
+        !User
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({
+            User,
+            //   grade: await grade(req.params.userID),
+            })
+      )
+      .catch((err) => {
+        console.log("borked", err);
+        return res.status(500).json(err);
+      });
+  },
+
+createUser(req, res) {
+    User.create(req.body)
+      .then((User) => res.json(User))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userID })
+      .then((User) =>
+        !User
+          ? res.status(404).json({ message: 'No such user exists' })
+          :res.json
+      )
+      .catch((err) => {
+        console.log("borked", err);
+        res.status(500).json(err);
+      });
+  },
+
+//   adding a friend..
+  newFriend(req, res) {
+    console.log('You are adding a new friend- make sure theyre cute');
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userID },
+      { $addToSet: { friends: req.params.friendId } },
+    //   { runValidators: true, new: true }
+    )
+      .then((User) =>
+        !User
+          ? res
+              .status(404)
+              .json({ message: 'No one found with that ID' })
+          : res.json(User)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userID },
+      { $pull: { friends:req.params.friendId } },
+    //   { runValidators: true, new: true }
+    )
+      .then((User) =>
+        !User
+          ? res
+              .status(404)
+              .json({ message: 'No User found with that ID' })
+          : res.json(User)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
